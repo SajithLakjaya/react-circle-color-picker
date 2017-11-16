cff5c908d7b7edcbadbbc047f8ab47842c512f8a
SajithLakjaya <sajith@ezeronetech.com>
2017-11-16 09:15:59 +0530

    change the project structure, remove welcome page, set entry point

--- src/components/color-picker.js
+++ src/components/color-picker.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import Circle from './components/circle';
+import Circle from './circle';
 
 class ReactCircleColorPicker extends React.Component {
 
--- stories/index.js
+++ stories/index.js
@@ -7,12 +7,10 @@ import { linkTo } from '@storybook/addon-links';
 import { Button, Welcome } from '@storybook/react/demo';
 
 import * as material from 'material-colors';
-import ReactCircleColorPicker from '../src/react-circle-color-picker';
-
-storiesOf('Welcome', module).add('to Storybook', () => <Welcome showApp={linkTo('Circle Color Picke')} />);
+import ReactCircleColorPicker from '../src';
 
 storiesOf('Circle Color Picker', module)
-  .add('Component', () => <ReactCircleColorPicker 
-    colors={[{hex: material.red[500], selected: true}, {hex: material.blue[500], selected: false}, {hex: material.green[500], selected: false}]}
+  .add('Component', () => <ReactCircleColorPicker
+    colors={[{ hex: material.red[500], selected: true }, { hex: material.blue[500], selected: false }, { hex: material.green[500], selected: false }]}
     onChange={action('onChange')}
-    />);
+  />);
